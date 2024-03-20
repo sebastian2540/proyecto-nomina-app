c1f11ca25463aa4cc232d2d7fffb4ef2f803fe50
Sebastian Villada Jaramillo <sebastian.villada.jaramillo@gmail.com.co>
2024-03-19 21:11:35 -0500

    [ADD] creación del menu

---
+++ app/src/main/java/com/example/nominacesde/HomeMenu.java
@@ -0,0 +1,7 @@
+package com.example.nominacesde;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class HomeMenu extends AppCompatActivity {
+
+}
+
--- app/src/main/java/com/example/nominacesde/MainActivity.java
+++ app/src/main/java/com/example/nominacesde/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.nominacesde;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView usuario, contrasena;
+    Button ingresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        usuario = findViewById(R.id.InputUsuario);
+        contrasena = findViewById(R.id.InputContrasena);
+        ingresar = findViewById(R.id.ButtonIngresar);
+
+        ingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                VerLogin verificar = new VerLogin(usuario.getText().toString(), contrasena.getText().toString());
+                if (verificar.validacioncorreo() == 1){
+                    Toast.makeText(MainActivity.this, "Bienvenido", Toast.LENGTH_SHORT).show();
+                    Intent seguir = new Intent(MainActivity.this, HomeMenu.class);
+                    startActivity(seguir);
+                } else {
+                    Toast.makeText(MainActivity.this, "Usuario y Contraseña incorrecto", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
