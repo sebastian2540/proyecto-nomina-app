c5fa81450c548e443c9dd3d2a83287c7fc573035
Sebastian Villada Jaramillo <sebastian.villada.jaramillo@gmail.com>
2024-05-31 23:55:18 -0500

    [CONFIG] se realizan configuraciones generales en el proyecto, se crea el formulario de registro, se configura la funcionalidad de los botones guardar y cancelar en los formularios de liquidación

--- app/src/main/java/com/example/nominacesde/Registrarse.java
+++ app/src/main/java/com/example/nominacesde/Registrarse.java
@@ -35,4 +35,5 @@ public class Registrarse extends AppCompatActivity {
         startActivity(inicio);
         Toast.makeText(getApplicationContext(), "Ha presionado el boton inicio", Toast.LENGTH_SHORT).show();
     }
+
 }
