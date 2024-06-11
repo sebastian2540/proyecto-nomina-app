e7d0c752c934d818e4f039ecca5f791bf1577967
Sebastian Villada Jaramillo <sebastian.villada.jaramillo@gmail.com>
2024-06-11 12:38:54 -0500

    [CONFIG] se agrega la funcionalidad de crear registros de los empleados y que se almacen en el Firestore

--- app/src/main/java/com/example/nominacesde/Empleado.java
+++ app/src/main/java/com/example/nominacesde/Empleado.java
@@ -2,10 +2,13 @@ package com.example.nominacesde;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -19,16 +22,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 
 public class Empleado extends AppCompatActivity {
 
+    private TextInputEditText editTextIdentificacion, editTextNombre, editTextApellido, editTextEmail, editTextCargo, editTextTipoContrato, editTextSalario, editTextIdEmpleado, editTextContrasena;
+    private Button buttonGuardar, btnCancelar;
     ImageButton btnMenu;
-//    Spinner listaAreas;
-//    String[] datosLista = {"Área", "Auxiliar Administrativo", "Contabilidad", "Nómina", "Sistemas"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,32 +64,121 @@ public class Empleado extends AppCompatActivity {
                 }
             }
         });
+        editTextIdentificacion = findViewById(R.id.input_identificacion_empleado);
+        editTextNombre = findViewById(R.id.input_nombre_empleado);
+        editTextApellido = findViewById(R.id.input_apellido_empleado);
+        editTextEmail = findViewById(R.id.input_email_empleado);
+        editTextCargo = findViewById(R.id.input_cargo_empleado);
+        editTextTipoContrato = findViewById(R.id.input_tipo_contrato_empleado_empleado);
+        editTextSalario = findViewById(R.id.input_salario_empleado);
+        editTextIdEmpleado = findViewById(R.id.input_id_empledo);
+        editTextContrasena = findViewById(R.id.input_contrasena_empleado);
+        buttonGuardar = findViewById(R.id.GuardarEmpleado);
+
+        // Agregar TextWatcher a editTextIdentificacion para copiar el texto a editTextIdEmpleado
+        editTextIdentificacion.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                editTextIdEmpleado.setText(s.toString());
+                editTextContrasena.setText(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        buttonGuardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String identificacion = editTextIdentificacion.getText().toString();
+                String nombre = editTextNombre.getText().toString();
+                String apellido = editTextApellido.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String cargo = editTextCargo.getText().toString();
+                String tipoContrato = editTextTipoContrato.getText().toString();
+                String salarioFloat = editTextSalario.getText().toString();
+                String idEmpleado = editTextIdEmpleado.getText().toString();
+                String contrasena = editTextContrasena.getText().toString();
+
+                float salario = Float.parseFloat(salarioFloat);
+
+                if(identificacion.isEmpty()){
+                    Toast.makeText(Empleado.this, "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                db.collection("tbl_empleados").whereEqualTo("id_empleado", identificacion).get().addOnCompleteListener(task -> {
+                    if(task.isSuccessful()) {
+                        QuerySnapshot querySnapshot = task.getResult();
+                        if(!querySnapshot.isEmpty()) {
+                            Toast.makeText(Empleado.this, "El usuario ya existe", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Map<String, Object> user = new HashMap<>();
+                            user.put("id_empleado", identificacion);
+                            user.put("nombre_empleado", nombre);
+                            user.put("apellido_empleado", apellido);
+                            user.put("email_empleado", email);
+                            user.put("cargo_empleado", cargo);
+                            user.put("tipo_contrato", tipoContrato);
+                            user.put("salario_empleado", salario);
+                            user.put("usuario", idEmpleado);
+                            user.put("contrasena", contrasena);
+
+                            db.collection("tbl_empleados")
+                                    .add(user)
+                                    .addOnSuccessListener(documentReference -> {
+                                        Toast.makeText(Empleado.this, "Usuario guardado con éxito", Toast.LENGTH_SHORT).show();
+                                        // Limpiar los campos después de guardar
+                                        editTextIdentificacion.setText("");
+                                        editTextNombre.setText("");
+                                        editTextApellido.setText("");
+                                        editTextEmail.setText("");
+                                        editTextCargo.setText("");
+                                        editTextTipoContrato.setText("");
+                                        editTextSalario.setText("");
+                                        editTextIdEmpleado.setText("");
+                                        editTextContrasena.setText("");
+                                    })
+                                    .addOnFailureListener(e -> Toast.makeText(Empleado.this, "Error al guardar usuario", Toast.LENGTH_SHORT).show());
+                        }
+                    } else {
+                        Toast.makeText(Empleado.this, "Error al obtener los datos", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
 
 
         btnMenu = (ImageButton) findViewById(R.id.imageButtonMenu);
         btnMenu.setOnClickListener(this::menu);
 
-//        listaAreas = (Spinner) findViewById(R.id.lista_areas_empleado);
-//        ArrayAdapter<String> adaptadorArea = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, datosLista);
-//        listaAreas.setAdapter(adaptadorArea);
-//
-//        listaAreas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//
-//            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//
-//            }
-//        });
+        btnCancelar = (Button) findViewById(R.id.CancelarEmpleado);
+        btnCancelar.setOnClickListener(this::cancelarEmpleado);
 
     }
     public void menu(View view) {
         Intent menu = new Intent(Empleado.this, Menu.class);
         startActivity(menu);
 //        Toast.makeText(getApplicationContext(), "Ha presionado el boton menu", Toast.LENGTH_SHORT).show();
+    }
+
+    public void cancelarEmpleado(View v) {
+        Intent cancelarEmpleado = new Intent(Empleado.this, Empleado.class);
+        // Limpiar los campos después de cancelar
+        editTextIdentificacion.setText("");
+        editTextNombre.setText("");
+        editTextApellido.setText("");
+        editTextEmail.setText("");
+        editTextCargo.setText("");
+        editTextTipoContrato.setText("");
+        editTextSalario.setText("");
+        editTextIdEmpleado.setText("");
+        editTextContrasena.setText("");
+        startActivity(cancelarEmpleado);
+//        Toast.makeText(getApplicationContext(), "Ha presionado el boton cancelar", Toast.LENGTH_SHORT).show();
     }
 
     @Override
